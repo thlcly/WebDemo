fcc41c1d93bab2b2aee4443557ee4acd197d178f
thlcly <thlcly@163.com>
2015-09-29 15:15:20 +0800

    add <csrf /> tag in spring security to avoid the scrf attack

--- SpringSecurityDemo/src/main/java/com/clean/demo/controller/LoginSuccessController.java
+++ SpringSecurityDemo/src/main/java/com/clean/demo/controller/LoginSuccessController.java
@@ -2,6 +2,8 @@ package com.clean.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by tonghui on 2015/9/25.
@@ -10,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("main")
 public class LoginSuccessController {
 
-    @RequestMapping("common")
+    @RequestMapping(value = "common",method = RequestMethod.GET)
     public String commonPage(){
         return "common";
     }
 
-    @RequestMapping("admin")
-    public String adminPage(){
+    @RequestMapping(value = "admin",method = RequestMethod.POST)
+    public String adminPage(@RequestParam(value = "name",required = false)String name,@RequestParam(value = "hobby",required = false)String hobby){
         return "admin";
     }
 
